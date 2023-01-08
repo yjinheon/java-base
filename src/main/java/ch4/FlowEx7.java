ccc606cb6cecb9adabaf174c211f74afe503ad42
jinheonyoon <yjinheon@gmail.com>
2023-01-08 17:43:29 +0900

    add gradle build setting

--- src/main/java/ch4/FlowEx7.java
+++ src/main/java/ch4/FlowEx7.java
@@ -1,8 +1,6 @@
 package ch4;
-
 import java.util.*;
 
-// nested if statements
 public class FlowEx7 {
     public static void main(String[] args) {
         System.out.print("Rock(1) Scissors(2) Paper(3) Game");
